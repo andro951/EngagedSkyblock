f26177dcacbec839f7f0eb4335ab2f21fdbc52fb
andro951 <andro951@gmail.com>
2023-10-15 04:07:23 -0400

    GlobalTile save before deleting troubleshooting logs

--- Common/Globals/ES_GlobalTile.cs
+++ Common/Globals/ES_GlobalTile.cs
@@ -17,6 +17,17 @@ using static EngagedSkyblock.EngagedSkyblock;
 
 namespace EngagedSkyblock.Common.Globals {
 	public class ES_GlobalTile : GlobalTile {
+		public override void Load() {
+			On_Player.ItemCheck_UseMiningTools_ActuallyUseMiningTool += On_Player_ItemCheck_UseMiningTools_ActuallyUseMiningTool;
+		}
+		internal static void OnWorldLoad() {
+			if (Main.netMode == NetmodeID.MultiplayerClient)
+				return;
+
+			SetupDesertFossils();
+		}
+
+		#region Tiles Hit by Hammer
 
 		public delegate bool orig_TileLoader_Drop(int x, int y, int type, bool includeLargeObjectDrops);
 		public delegate bool hook_TileLoader_Drop(orig_TileLoader_Drop orig, int x, int y, int type, bool includeLargeObjectDrops);
@@ -38,9 +49,6 @@ namespace EngagedSkyblock.Common.Globals {
 			}
 			
 			return orig(x, y, type, includeLargeObjectDrops);
-		}
-		public override void Load() {
-			On_Player.ItemCheck_UseMiningTools_ActuallyUseMiningTool += On_Player_ItemCheck_UseMiningTools_ActuallyUseMiningTool;
 		}
 		private static SortedDictionary<int, Point> tilesJustHit = new();
 		private void On_Player_ItemCheck_UseMiningTools_ActuallyUseMiningTool(On_Player.orig_ItemCheck_UseMiningTools_ActuallyUseMiningTool orig, Player self, Item sItem, out bool canHitWalls, int x, int y) {
@@ -68,6 +76,11 @@ namespace EngagedSkyblock.Common.Globals {
 			modPacket.Write(playerWhoAmI);
 			modPacket.Send();
 		}
+
+		#endregion
+
+		#region Random Update and Place Tile
+
 		private static int defaultTileToPlace = -1;
 		public override void RandomUpdate(int i, int j, int type) {
 			if (!ES_WorldGen.SkyblockWorld)
@@ -113,6 +126,11 @@ namespace EngagedSkyblock.Common.Globals {
 			if (Main.netMode == NetmodeID.Server)
 				NetMessage.SendTileSquare(-1, i - 1, j - 1, 3);
 		}
+
+		#endregion
+
+		#region Ice Spread
+
 		private bool IceCanSpread(ref int i, ref int j, out int iceType) {
 			iceType = -1;
 			Tile tile = Main.tile[i, j];
@@ -156,6 +174,11 @@ namespace EngagedSkyblock.Common.Globals {
 
 			return false;
 		}
+
+		#endregion
+
+		#region Desert Fossils
+
 		private static SortedSet<Point> burriedOrganicTiles = new(comparer: new Compairers.PointComparer());
 		private static float onDistanceThreshold = 16f * 30f;
 		private static float offDistanceThreshold = 16f * 20f;
@@ -372,6 +395,11 @@ namespace EngagedSkyblock.Common.Globals {
 				burriedOrganicTiles.Add(point);
 			}
 		}
+
+		#endregion
+
+		#region Sand Hardening
+
 		public static int sandAboveToHarden = 4;
 		private static SortedDictionary<int, int> SandToHardendedSand = new() {
 			{ TileID.Sand, TileID.HardenedSand },
@@ -404,6 +432,11 @@ namespace EngagedSkyblock.Common.Globals {
 
 			return true;
 		}
+
+		#endregion
+
+		#region Mud Conversion
+
 		public static bool MudThatCanConvertToClay(int i, int j) {
 			if (Main.raining)
 				return false;
@@ -413,26 +446,8 @@ namespace EngagedSkyblock.Common.Globals {
 				int tileType = tile.TileType;
 				if (tileType == TileID.Mud) {
 					if (TouchingAir(i, j)) {
-						bool waterNearby = false;
-						int radius = 4;
-						int xStart = Math.Max(0, i - radius);
-						int xEnd = Math.Min(Main.maxTilesX - 1, i + radius);
-						for (int x = xStart; x <= xEnd; x++) {
-							int yStart = Math.Max(0, j - radius);
-							int yEnd = Math.Min(Main.maxTilesY - 1, j + radius);
-							for (int y = yStart; y <= yEnd; y++) {
-								Tile t = Main.tile[x, y];
-								if (t.LiquidAmount > 0 && t.LiquidType == LiquidID.Water) {
-									if (HasPath(i, j, x, y, (float)radius + 0.99f, CountsAsMudPath)) {
-										waterNearby = true;
-										goto finishedWaterNearbyCheck;
-									}
-								}
-							}
-						}
-
-					finishedWaterNearbyCheck:
-						return !waterNearby;
+						int radius = 10;
+						return !HasMudPathToWater(i, j, radius);
 					}
 				}
 			}
@@ -445,7 +460,198 @@ namespace EngagedSkyblock.Common.Globals {
 			TileID.Grass,
 			TileID.Dirt
 		};
-		private static bool CountsAsMudPath(Tile tile) => tile.HasTile && mudPathAllowedTiles.Contains(tile.TileType) || tile.LiquidAmount > 0 && tile.LiquidType == LiquidID.Water;
+		private static bool HasMudPathToWater(int i, int j, int radius) {
+			return HasPath(i, j, radius, CountsAsMudPath, CountsAsMudWaterSource, Main.maxTilesX, Main.maxTilesY);
+		}
+		private static bool CountsAsMudPath(int x, int y) {
+			Tile tile = Main.tile[x, y];
+			return tile.HasTile && mudPathAllowedTiles.Contains(tile.TileType);
+		}
+		private static bool CountsAsMudWaterSource(int x, int y) {
+			Tile tile = Main.tile[x, y];
+			return tile.LiquidAmount > 0 && tile.LiquidType == LiquidID.Water;
+		}
+		private static void GetBoundaries(int x, int y, int xMin, int xMax, int yMin, int yMax, int radius, out int left, out int up, out int right, out int down) {
+			left = Math.Max(-radius, xMin - x);
+			up = Math.Max(-radius, yMin - y);
+			right = Math.Min(radius, xMax - x);
+			down = Math.Min(radius, yMax - y);
+		}
+		public static bool HasPath(int x, int y, int MaxDistance, Func<int, int, bool> CountsAsPath, Func<int, int, bool> CountsAsTarget, int XMax, int YMax, int XMin = 0, int YMin = 0) {
+			countsAsPath = CountsAsPath;
+			countsAsTarget = CountsAsTarget;
+			maxDistance = MaxDistance;
+			//Create a rectangle that is either smaller than or equal to min and max limits provided, minimizing the size of PathGrid.
+			GetBoundaries(x, y, XMin, XMax, YMin, YMax, maxDistance, out int left, out int up, out int right, out int down);
+			//If not limited by the min or max values, left will be negative maxDistance, right positive maxDistance, up negative maxDistance, down positive maxDistance.
+			int gridSizeX = -left + right + 1;
+			int gridSizeY = -up + down + 1;
+			//centerX and centerY are the converted x and y coordinates in PathGrid.
+			centerX = -left;
+			centerY = -up;
+			//xStart and yStart are the difference between the provided x and y and the center of PathGrid.
+			//In FindPath(), x and y are in PathGrid coordinates.  x + xStart, y + yStart will be in the original coordinates.
+			xStart = x - centerX;
+			yStart = y - centerY;
+			xMax = gridSizeX - 1;
+			xMin = 0;
+			yMax = gridSizeY - 1;
+			yMin = 0;
+			//Resizes PathGrid to gridSizeX by gridSizeY, and sets all values to int.MaxValue
+			FillArray(ref PathGrid, gridSizeX, gridSizeY, int.MaxValue);
+			//Set the starting point to 0 to prevent paths from trying to go back through it.
+			PathGrid[centerX, centerY] = 0;
+
+			bool hasPath = FindPath(centerX, centerY, 0);
+
+			PathGrid = null;
+			//if (Debugger.IsAttached && resultPath != null) resultPath.LogSimpleNT();
+			resultPath = null;
+			countsAsPath = null;
+			countsAsTarget = null;
+
+			return hasPath;
+		}
+		private static int[,] PathGrid;
+		private static string resultPath;
+		private static int xStart;
+		private static int yStart;
+		private static int centerX;
+		private static int centerY;
+		private static Func<int, int, bool> countsAsPath;
+		private static Func<int, int, bool> countsAsTarget;
+		private static int maxDistance;
+		private static int xMax;
+		private static int yMax;
+		private static int xMin;
+		private static int yMin;
+		/// <summary>
+		/// Searches for a path to a position satisfied by countsAsTarget only through positions satisfied by countsAsPath.<br/>
+		/// </summary>
+		private static bool FindPath(int x, int y, int currentDistance, int fromDirection = -1, int previousFrom = -1) {
+			//opposite and previousOpposite are the opposite directionIDs for the path taken to get to this point.
+			//For instance, if the path taken to get here was directionID 0 (down), opposite will be 2 (up).
+			//previousFrom is used to track the previous x if fromDirection is tracking y or vice versa because the path should generally go away from previous paths.
+			int opposite = fromDirection >= 0 ? (fromDirection + 2) % 4 : -1;
+			int previousOpposite = previousFrom >= 0 ? (previousFrom + 2) % 4 : -1;
+			for (int directionID = 0; directionID < 4; directionID++) {
+				int i = directionID % 2;
+				int j = 1 - i;
+				if (directionID > 1) {
+					i *= -1;
+					j *= -1;
+				}
+
+				//directionID:  i,  j
+				//down		0:  0,  1
+				//right		1:  1,  0
+				//up		2:  0, -1
+				//left		3: -1,  0
+
+				if (opposite == directionID)
+					continue;
+
+				int x2 = x + i;
+				//Prevent out of bounds
+				if (x2 < xMin || x2 > xMax)
+					continue;
+
+				int y2 = y + j;
+				//Prevent out of bounds
+				if (y2 < yMin || y2 > yMax)
+					continue;
+
+				//Increment the distance every time we move to a new position.
+				int distance = currentDistance + 1;
+				//PathGrid stores the distance required to get to this point by previous paths or int.MaxValue if not yet reached.
+				//There is no reason to continue checking a path on a tile that has been reached by a shorter path.
+				if (PathGrid[x2, y2] <= distance)
+					continue;
+
+				if (distance > maxDistance)
+					continue;
+
+				//realX and realY are converting the search area coordinates to coordinates of the original FindPath() request.
+				int realX = x2 + xStart;
+				int realY = y2 + yStart;
+
+				//Base case
+				if (countsAsTarget(realX, realY))
+					return true;
+
+				//Mark the grid with the distance required to get to this point on this path.
+				PathGrid[x2, y2] = distance;
+
+				if (!countsAsPath(realX, realY))
+					continue;
+
+				//Usually, the searches should only go out in 2 directions, an x direction and a y direction.
+				//However, there are some situations where backtracking is the only option, so only skip the backtrack path if the
+				//	backTrackX, backTrackY position isn't the starting point and doesn't count as a path.
+				if (previousOpposite == directionID) {
+					GetPreviousDirection(x + xStart, y + yStart, fromDirection, previousFrom, out int backTrackX, out int backTrackY);
+					if (backTrackX == centerX && backTrackY == centerY || countsAsPath(backTrackX, backTrackY))
+						continue;
+				}
+
+				if (FindPath(x2, y2, distance, directionID, fromDirection == directionID ? previousFrom : fromDirection)) {
+					if (Debugger.IsAttached) {
+						//resultPath is printed when the HasPath finishes.
+						if (resultPath == null) {
+							resultPath = $"({x2}, {y2})";
+						}
+						else {
+							resultPath += $", ({x2}, {y2})";
+						}
+					}
+
+					return true;
+				}
+			}
+			return false;
+		}
+		/// <summary>
+		/// Prevents the path from going almost in a circle by doing something such as down 1, left 1, up 1 if the previous coordinates count as a path.<br/>
+		/// x0 and y0 are the coordinates of the current search.  x and y will be back 1 in both of the previous directions.<br/>
+		/// . . 3 0<br/>
+		/// . . 2 1<br/>
+		/// . . . .<br/>
+		/// . . . .<br/>
+		/// Prevents the path to 3 because it can be reached by a shorter path from the starting point, 0.
+		/// </summary>
+		private static void GetPreviousDirection(int x0, int y0, int fromDirection, int previousFrom, out int x, out int y) {
+			//directionID:  i,  j
+			//down		0:  0,  1
+			//right		1:  1,  0
+			//up		2:  0, -1
+			//left		3: -1,  0
+			x = x0;
+			y = y0;
+			bool fromPositiveY = fromDirection == 0 || previousFrom == 0;
+			bool fromPositiveX = fromDirection == 1 || previousFrom == 1;
+			if (fromPositiveY) {
+				y--;
+			}
+			else {
+				y++;
+			}
+
+			if (fromPositiveX) {
+				x--;
+			}
+			else {
+				x++;
+			}
+		}
+		public static void FillArray<T>(ref T[,] arr, int xLen, int yLen, T value) {
+			arr = new T[xLen, yLen];
+			for (int y = 0; y < yLen; y++) {
+				for (int x = 0; x < xLen; x++) {
+					arr[x, y] = value;
+				}
+			}
+		}
+		//private static bool CountsAsMudPath(Tile tile) => tile.HasTile && mudPathAllowedTiles.Contains(tile.TileType) || tile.LiquidAmount > 0 && tile.LiquidType == LiquidID.Water;
 		private static bool TouchingAir(int i, int j) {
 			for (int k = 0; k < DirectionID.Count; k++) {
 				int x = i;
@@ -461,116 +667,8 @@ namespace EngagedSkyblock.Common.Globals {
 
 			return false;
 		}
-		public static bool HasPath(int x, int y, int targetX, int targetY, float maxDistance, Func<Tile, bool> countsAsPath) {
-			if (x == targetX && y == targetY)
-				return true;
 
-			return FindPath(x, y, x, y, targetX, targetY, maxDistance, countsAsPath, currentDistance: Distance(x, y, targetX, targetY));
-		}
-		public class Element<K, T> where K : IComparable {
-			public Element(K key, T value, Element<K, T> prev = null, Element<K, T> next = null) {
-				this.key = key;
-				this.value = value;
-				this.prev = prev;
-				this.next = next;
-			}
+		#endregion
 
-			public  K key;
-			public T value;
-			public Element<K, T> next;
-			public Element<K, T> prev;
-		}
-		public class OrderList<K, T> where K : IComparable {
-			public OrderList() {}
-			public Element<K, T> first = null;
-			public Element<K, T> last = null;
-			public void Add(K key, T value) {
-				if (first == null) {
-					first = new(key, value);
-					last = first;
-					return;
-				}
-
-				for (Element<K, T> current = first; current != null; current = current.next) {
-					if (current.key.CompareTo(key) < 0) {
-						Element<K, T> newElement = new(key, value, current.prev, current);
-						current.prev = newElement;
-						if (newElement.prev != null) {
-							newElement.prev.next = newElement;
-						}
-						else {
-							first = newElement;
-						}
-
-						return;
-					}
-				}
-
-				last.next = new(key, value, last);
-				last = last.next;
-			}
-		}
-		public delegate bool FindPathDelegate(int xStart, int yStart, int x, int y, int targetX, int targetY, float maxDistance, Func<Tile, bool> countsAsPath);
-		private static bool FindPath(int xStart, int yStart, int x, int y, int targetX, int targetY, double maxDistance, Func<Tile, bool> countsAsPath, int from = DirectionID.None, double currentDistance = 0d) {
-			//Filter and put into a list and sort by distance
-			double[] distances = new double[DirectionID.Count];
-			OrderList<double, int> order = new();
-			int acceptableDistanceCount = 0;//Used to check if any of the directions are outside the maxDistance.
-			for (int i = DirectionID.Up; i < DirectionID.Count; i++) {
-				//Don't check the direction it just came from.
-				if (i == from)
-					continue;
-
-				int x2 = x;
-				int y2 = y;
-				DirectionID.ApplyDirection(ref x2, ref y2, i);
-				//Base Case
-				if (x2 == targetX && y2 == targetY)
-					return true;
-
-				ref double distance = ref distances[i];
-				distance = Distance(x2, y2, targetX, targetY);
-				if (distance <= maxDistance)
-					acceptableDistanceCount++;
-			}
-
-			bool outsideBorder = acceptableDistanceCount < 3;
-			for (int i = DirectionID.Up; i < DirectionID.Count; i++) {
-				//Don't check the direction it just came from.
-				if (i == from)
-					continue;
-
-				double distance = distances[i];
-				int x2 = x;
-				int y2 = y;
-				DirectionID.ApplyDirection(ref x2, ref y2, i);
-				if (!WorldGen.InWorld(x2, y2))
-					continue;
-
-				//Only include ones that are further than the current distance if the current position is on the outside border.
-				if (distance <= maxDistance && (distance <= currentDistance || outsideBorder) && countsAsPath(Main.tile[x2, y2]))
-					order.Add(distance, i);
-			}
-
-			for (Element<double, int> current = order.first; current != null; current = current.next) {
-				int x2 = x;
-				int y2 = y;
-				DirectionID.ApplyDirection(ref x2, ref y2, current.value);
-				if (FindPath(xStart, yStart, x2, y2, targetX, targetY, maxDistance, countsAsPath, DirectionID.GetOppositeDirection(current.value), current.key))
-					return true;
-			}
-
-			return false;
-		}
-		private static double Distance(int x1, int y1, int x2, int y2) {
-			return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
-		}
-
-		internal static void OnWorldLoad() {
-			if (Main.netMode == NetmodeID.MultiplayerClient)
-				return;
-
-			SetupDesertFossils();
-		}
 	}
 }
